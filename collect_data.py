11b1d0282979c0c82afc7ba5e4c1c623be515a23
Alex Schnapper <alexschnapper@gmail.com>
2025-02-17 11:50:05 +0100

    fixing things

--- collect_data.py
+++ collect_data.py
@@ -10,6 +10,7 @@ load_dotenv()
 access_token = os.getenv('ACCESS_TOKEN')
 user_id = os.getenv('USER_ID')
 url = f"https://graph.instagram.com"
+url2 = f"https://graph.facebook.com"
 
 def fetch_instagram_data():
     # Daten von der Instagram API abrufen
@@ -18,7 +19,7 @@ def fetch_instagram_data():
 
     # Daten in eine CSV-Datei speichern
     df = pd.DataFrame(data['data'])
-    df.to_csv('instagram_data.csv', index=False)
+    df.to_csv('data/instagram_data.csv', index=False)
 
     # Daten analysieren für Posts pro Monat
     df['timestamp'] = pd.to_datetime(df['timestamp'])
@@ -26,12 +27,19 @@ def fetch_instagram_data():
     posts_per_month = df.groupby('year_month').size()
 
     # Ergebnisse speichern
-    posts_per_month.to_csv('posts_per_month.csv')
+    posts_per_month.to_csv('data/posts_per_month.csv')
 
+def fetch_instagram_insights_data():
+    # Daten von der Instagram API abrufen
+    response = requests.get(f'{url2}/{user_id}/insights?metric=engagement,impressions,reach')
+    data = response.json()
+    # Daten in eine CSV-Datei speichern
+    df = pd.DataFrame(data['data'])
+    df.to_csv('data/instagram_insights_data.csv', index=False)
 
 def fetch_instagram_other():
     # Abrufen der Medien-Daten
-    media_url = f"https://graph.instagram.com/{USER_ID}/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username,like_count,comments_count&access_token={ACCESS_TOKEN}"
+    media_url = f"https://graph.instagram.com/{USER_ID}/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username,like_count,comments_count,account_type,followers_count,follows_count,media_count&access_token={ACCESS_TOKEN}"
     media_response = requests.get(media_url)
     media_data = media_response.json()
 
@@ -48,22 +56,22 @@ def fetch_instagram_other():
     followers_df = pd.DataFrame([{'timestamp': pd.Timestamp.now(), 'followers_count': followers_count}])
 
     # Speichern der Daten
-    posts_df.to_csv('instagram_data.csv', index=False)
+    posts_df.to_csv('data/instagram_data.csv', index=False)
 
     # Follower-Daten anhängen, wenn die Datei bereits existiert
-    if os.path.exists('instagram_followers.csv'):
+    if os.path.exists('data/instagram_followers.csv'):
         existing_followers_df = pd.read_csv('instagram_followers.csv')
         existing_followers_df['timestamp'] = pd.to_datetime(existing_followers_df['timestamp'])
         followers_df = pd.concat([existing_followers_df, followers_df], ignore_index=True)
 
-    followers_df.to_csv('instagram_followers.csv', index=False)
+    followers_df.to_csv('data/instagram_followers.csv', index=False)
     
     # Interaktionen pro Account berechnen
     posts_df['interactions'] = posts_df['like_count'] + posts_df['comments_count']
     interactions_per_account = posts_df.groupby('username')['interactions'].sum().reset_index()
 
     # Speichern der aufbereiteten Daten
-    interactions_per_account.to_csv('interactions_per_account.csv', index=False)
+    interactions_per_account.to_csv('data/interactions_per_account.csv', index=False)
 
     # Verweise (Mentions) extrahieren
     mentions = []
@@ -78,9 +86,10 @@ def fetch_instagram_other():
     mentions_count.columns = ['mentioned_account', 'count']
 
     # Speichern der Verweise (Mentions)
-    mentions_count.to_csv('mentions_count.csv', index=False)
+    mentions_count.to_csv('data/mentions_count.csv', index=False)
     
     
 if __name__ == "__main__":
     fetch_instagram_data()
-    fetch_instagram_other
+    fetch_instagram_insights_data()
+    fetch_instagram_other()
